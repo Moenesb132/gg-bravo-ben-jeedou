1cc7aae0c85dc0cb1173c64af66f00550d454ccb
Moenesb132 <70076618+Moenesb132@users.noreply.github.com>
2020-08-22 23:59:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['703609180858089562']
+const myid = ['630122446133133353']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
